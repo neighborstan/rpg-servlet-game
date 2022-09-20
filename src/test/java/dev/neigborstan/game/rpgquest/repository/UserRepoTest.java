ca18a2a8bb1186c30bf233c5113bd860c84a218d
neighborstan <dropweb.post@gmail.com>
2022-09-20 19:46:43 +0300

    added test method

--- src/test/java/dev/neigborstan/game/rpgquest/repository/UserRepoTest.java
+++ src/test/java/dev/neigborstan/game/rpgquest/repository/UserRepoTest.java
@@ -2,7 +2,11 @@ package dev.neigborstan.game.rpgquest.repository;
 
 import dev.neigborstan.game.rpgquest.entity.User;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,11 +29,21 @@ class UserRepoTest {
     @Test
     void restartUser() {
 //        НЕ СМОГ (
-
     }
 
     @Test
     void deleteUser() {
+        UserRepo userRepo = new UserRepo();
+        List<User> users = new ArrayList<>();
+        IntStream.rangeClosed(1, 3)
+                .forEach(num -> users.add(new User(num, "name" + num, num, Mockito.mock(LocationRepo.class))));
+
+        userRepo.getUsers().addAll(users);
+
+        User name = users.get(1);
+        userRepo.deleteUser(name);
+
+        assertFalse(userRepo.getUsers().contains(name));
     }
 
     @Test
